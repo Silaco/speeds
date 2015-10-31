f16d68777c6366eb3d48e59f4641a9ff7da4ea49
Prakash <rebornindian@example.com>
2015-10-31 22:27:25 +0530

    Upgrades

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import url
 from mysite.views import MyView
 from mysite.views import CurrentClass
 from mysite.views import Simple
-
+AnsibleInvoke
 
 
 urlpatterns = [
     url(r'^hello/$', MyView.as_view(), name='my-view'),
 	url(r'^current/$', CurrentClass.as_view(), name='my-current'),
-	url(r'^search-form/$', Simple.search_form),	
+	url(r'^search-form/$', Simple.search_form),
+	url(r'^invoke/$', AnsibleInvoke.Invoke),
     url(r'^search/$', Simple.search),
 ]
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -22,25 +22,8 @@ class CurrentClass(View):
 		context=Context({'Test':1})
 		return HttpResponse(template.render(context))
 
-import ansible.runner
-
 class Simple(View):		
 	def search_form(request):
-		# results = ansible.runner.Runner(
-		   # module_name='ping',
-		   # module_args='',
-		   # pattern='*',
-		   # forks=10
-		# ).run()
-		# results=runner.run()
-		
-		# html = []
-		
-		# html.append('<tr><td>Value</td><td>'++'</td></tr>')
-		
-		# for (hostname, result) in results['contacted'].items():			
-			# html.append('<tr><td>'+hostname+'</td><td>'+result['stdout']+'</td></tr>')
-		
 		return render(request, 'search_form.htm')
 	def search(request):
 		if 'id' in request.GET:
@@ -64,47 +47,23 @@ class Simple(View):
 			message = 'You submitted an empty form.'
 		return HttpResponse(message)
 		
+class AnsibleInvoke(View):		
+	def Invoke(request):
+		
+		import ansible.runner
 
-	def get(request):
+		runner = ansible.runner.Runner(
+		   module_name='ping',
+		   module_args='',
+		   pattern='web*',
+		   forks=10
+		)
+		datastructure = runner.run()
 		
-		# import ansible.runner
-
-		# runner = ansible.runner.Runner(
-		   # module_name='ping',
-		   # module_args='',
-		   # pattern='*',
-		   # forks=10
-		# )
-		# results=runner.run()
-		
-		# html = []
-		
-		# html.append('<tr><td>Value</td><td>'++'</td></tr>')
-		
-		# for (hostname, result) in results['contacted'].items():			
-			# html.append('<tr><td>'+hostname+'</td><td>'+result['stdout']+'</td></tr>')
-		
-		# return HttpResponse('<table>%s</table>' % '\n'.join(html))
+		html = []
+		for row in cursor:
+			html.append('<tr><td>'+str(row[0])+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+str(row[3])+'</td></tr>')
+		conn.close()			
+		return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
 		return render(request, 'search_form.htm')
-	def search(request):
-		if 'id' in request.GET:
-			name=request.GET['name']
-			id=request.GET['id']
-			address=request.GET['address']
-			salary=request.GET['salary']	
-			
-			# conn = sqlite3.connect('test.db')
-			
-			# conn.execute();
-			
-			sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES ("+id+", '"+name+"', 32, '"+address+"', "+salary+" )"
-			
-			conn = sqlite3.connect('test.db')
-			
-			conn.execute(sql);
-			conn.commit();
-			message = sql
-		else:
-			message = 'You submitted an empty form.'
-		return HttpResponse(message)
