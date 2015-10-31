0d8b5f6c755197ff9b5a7682a218053888c7845d
Prakash <rebornindian@example.com>
2015-11-01 02:11:26 +0530

    Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,11 +8,11 @@ from mysite.views import AnsibleInvoke1
 
 
 urlpatterns = [
-    url(r'^hello/$', MyView.as_view(), name='my-view'),
-	url(r'^current/$', CurrentClass.as_view(), name='my-current'),
-	url(r'^search-form/$', Simple.search_form),
-	url(r'^invoke/$', AnsibleInvoke.Invoke),
-    url(r'^search/$', Simple.search),
+    # url(r'^hello/$', MyView.as_view(), name='my-view'),
+	# url(r'^current/$', CurrentClass.as_view(), name='my-current'),
+	# url(r'^search-form/$', Simple.search_form),
+	# url(r'^invoke/$', AnsibleInvoke.Invoke),
+    # url(r'^search/$', Simple.search),
 	url(r'^run/$', AnsibleInvoke1.as_view(), name='new-view11'),
 ]
 
