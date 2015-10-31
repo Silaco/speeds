6df52f01202b4502b0ed479af98d0705303e98ba
Prakash <rebornindian@example.com>
2015-10-31 23:32:29 +0530

    Update

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -16,21 +16,61 @@ class MyView(View):
 
 class AnsibleInvoke1(View):
 	def get(self, request, *args, **kwargs):		
-		import ansible.runner
+		from ansible.playbook import PlayBook
+		from ansible.inventory import Inventory
+		from ansible import callbacks
+		from ansible import utils
 
-		runner = ansible.runner.Runner(
-		   module_name='ping',
-		   module_args='',
-		   pattern='web*',
-		   forks=10
+		import jinja2
+		from tempfile import NamedTemporaryFile
+		import os
+
+		# Boilerplace callbacks for stdout/stderr and log output
+		utils.VERBOSITY = 0
+		playbook_cb = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY)
+		stats = callbacks.AggregateStats()
+		runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
+
+		# Dynamic Inventory
+		# We fake a inventory file and let Ansible load if it's a real file.
+		# Just don't tell Ansible that, so we don't hurt its feelings.
+		inventory = """
+		[current]
+		{{ public_ip_address }}
+		"""
+
+		inventory_template = jinja2.Template(inventory)
+		rendered_inventory = inventory_template.render({
+			'public_ip_address': '111.222.333.444'    
+			# and the rest of our variables
+		})
+
+		# Create a temporary file and write the template string to it
+		hosts = NamedTemporaryFile(delete=False)
+		hosts.write(rendered_inventory)
+		hosts.close()
+
+		pb = PlayBook(
+			playbook='/home/ec2-user/hack/speeds/mysite/py.yaml',
+			host_list=hosts.name,     # Our hosts, the rendered inventory file    
+			callbacks=playbook_cb,
+			runner_callbacks=runner_cb,
+			stats=stats,
+			# private_key_file='/path/to/key.pem'
 		)
-		datastructure = runner.run()
+
+		results = pb.run()
+
+		# Ensure on_stats callback is called
+		# for callback modules
+		playbook_cb.on_stats(pb.stats)
+
+		os.remove(hosts.name)
+
+		print results
 		
-		html = []
-		for row in cursor:
-			html.append('<tr><td>'+str(row[0])+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+str(row[3])+'</td></tr>')
-		# conn.close()			
-		return HttpResponse('<table>%s</table>' % '\n'.join(html))
+		return HttpResponse(results)
+
 
 class CurrentClass(View):
 	def get(self, request, *args, **kwargs):	
