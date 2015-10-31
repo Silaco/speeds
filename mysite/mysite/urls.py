55752f59b0ff1e8a9be3cd2a0585acd41d9e2dc9
Prakash <rebornindian@example.com>
2015-10-31 19:17:37 +0530

    Upgrades

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import url
 
 from mysite.views import MyView
 from mysite.views import CurrentClass
+from mysite.views import Simple
+
 
 urlpatterns = [
     url(r'^hello/$', MyView.as_view(), name='my-view'),
 	url(r'^current/$', CurrentClass.as_view(), name='my-current'),
+	url(r'^search-form/$', Simple.search_form),
+    url(r'^search/$', Simple.search),
 ]
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.views.generic import View
+from django.shortcuts import render
+import sqlite3
 
 class MyView(View):
+	def get(self, request, *args, **kwargs):		
+		conn = sqlite3.connect('test.db')
+		cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
+		html = []
+		for row in cursor:
+			html.append('<tr><td>'+str(row[0])+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+str(row[3])+'</td></tr>')
+		conn.close()			
+		return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, World!')
 class CurrentClass(View):
 	def get(self, request, *args, **kwargs):	
 		import os.path
@@ -13,3 +21,28 @@ class CurrentClass(View):
 		template=loader.get_template('ShowTime.htm')
 		context=Context({'Test':1})
 		return HttpResponse(template.render(context))
+
+class Simple(View):		
+	def search_form(request):
+		return render(request, 'search_form.htm')
+	def search(request):
+		if 'id' in request.GET:
+			name=request.GET['name']
+			id=request.GET['id']
+			address=request.GET['address']
+			salary=request.GET['salary']	
+			
+			# conn = sqlite3.connect('test.db')
+			
+			# conn.execute();
+			
+			sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES ("+id+", '"+name+"', 32, '"+address+"', "+salary+" )"
+			
+			conn = sqlite3.connect('test.db')
+			
+			conn.execute(sql);
+			conn.commit();
+			message = sql
+		else:
+			message = 'You submitted an empty form.'
+		return HttpResponse(message)
