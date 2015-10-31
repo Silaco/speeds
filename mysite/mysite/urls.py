bccd25eea0f439db3906046d95c5b95d23079c78
Prakash <rebornindian@example.com>
2015-11-01 02:58:36 +0530

    Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url
 # from mysite.views import MyView
 # from mysite.views import CurrentClass
 # from mysite.views import Simple
-from mysite.views import LoggedIn as Log
+from mysite.views import LoggedIn
 from mysite.views import AnsibleInvoke1
 
 
 urlpatterns = [
     # url(r'^hello/$', MyView.as_view(), name='my-view'),
 	# url(r'^current/$', CurrentClass.as_view(), name='my-current'),
-	url(r'^LoggedIn/$', Log.Login),
+	url(r'^LoggedIn/Login$', LoggedIn.Login, name='LoggedIn'),
 	# url(r'^invoke/$', AnsibleInvoke.Invoke),
     # url(r'^search/$', Simple.search),
 	url(r'^run/$', AnsibleInvoke1.as_view(), name='new-view11'),
