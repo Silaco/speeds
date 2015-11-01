9c3f1ba51b248e0e26916b01028fee8c76a7b498
Prakash <rebornindian@example.com>
2015-11-01 20:09:03 +0530

    Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -10,7 +10,7 @@ from mysite.views import Login,get,login,index,check,saveHost,Hosts,audit,home
 urlpatterns = [
 	url(r'^Login/$', Login, name='my-view'),
 	url(r'^run/$', get, name='new-view11'),
-	url(r'^index/$', index, name='new-view11q'),
+	url(r'^index/$', home, name='new-view11q'),
 	url(r'^login/$', login, name='login-view11'),
 	url(r'^RDM/$', check, name='check'),
 	# url(r'home^', Login, name='home'),
